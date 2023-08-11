723027c1994dc0456a538b09c7a4d04928d398c9
yisak <yisakbruck23@gmail.com>
2023-08-11 16:39:46 -0400

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,37 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The provided object is not of type Pet.")
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type provided.")
+        self.pet_type = pet_type
+
+        self.owner = owner
+        if owner:
+            owner.add_pet(self)
+
+        Pet.all.append(self)
+
+    def __repr__(self):  
+        return self.name
